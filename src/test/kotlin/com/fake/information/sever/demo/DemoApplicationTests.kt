ee6532f759a3e1d0a85a7de1809333d3c03b022a
farewell12345 <1149558764@qq.com>
2021-01-17 18:45:17 +0800

    初始化仓库

---
+++ src/test/kotlin/com/fake/information/sever/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fake.information.sever.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
