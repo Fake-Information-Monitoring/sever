1ccf643003e08f6cf93f1da6c852e3e36ddbbf5e
Umb <1149558764@qq.com>
2021-04-05 16:23:01 +0800

    Async,我的超人

--- src/main/kotlin/com/fake/information/sever/demo/Until/VerifyCode/VerifyCode.kt
+++ src/main/kotlin/com/fake/information/sever/demo/Until/VerifyCode/VerifyCode.kt
@@ -4,28 +4,33 @@ import cn.hutool.captcha.CaptchaUtil
 import cn.hutool.captcha.ShearCaptcha
 import com.fake.information.sever.demo.Redis.FakeNewsRedisTemplate
 import kotlinx.coroutines.*
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.TimeUnit
 import javax.servlet.http.HttpSession
-
+import com.fake.information.sever.demo.Until.AsyncTask.AsyncService
 /*
 * @TODO:验证码生成以及验证
 */
 @RestController
 class VerifyCode(private var redisTemplate: FakeNewsRedisTemplate) {
+    @Autowired
+    private lateinit var asyncService: AsyncService
+
 
     @ObsoleteCoroutinesApi
     fun createCode(session: HttpSession, subject: String): ShearCaptcha {
+
         val captcha: ShearCaptcha = CaptchaUtil
                 .createShearCaptcha(200, 100, 4, 10)
-//        FakeNewsAsyncService().asyncTask {
-//            redisTemplate.setRedis(session.id + subject, captcha)
-//            redisTemplate.setTime(
-//                session.id + subject,
-//                1000 * 300,
-//                TimeUnit.SECONDS
-//            )
-//        }
+        asyncService.asyncTask {
+            redisTemplate.setRedis(session.id + subject, captcha)
+            redisTemplate.setTime(
+                session.id + subject,
+                1000 * 300,
+                TimeUnit.SECONDS
+            )
+        }
         return captcha
     }
 
