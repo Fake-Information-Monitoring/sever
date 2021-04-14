49c826a3cf359952e1066ed8738cb73bb280c461
Umb <1149558764@qq.com>
2021-04-14 17:06:21 +0800

    跨域发送cookie

--- src/main/kotlin/com/fake/information/sever/demo/Http/Interceptor/Interceptor.kt
+++ src/main/kotlin/com/fake/information/sever/demo/Http/Interceptor/Interceptor.kt
@@ -57,7 +57,7 @@ class Interceptor : WebMvcConfigurer {
         override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
             response.addHeader("Access-Control-Allow-Credentials","true")
             val session = request.session
-            if (redisTemplate.getRedis(session.id) == StatusCode.Status200.statusCode) {//TODO: session状态码拦截
+            if (redisTemplate.getRedis(session.id) == StatusCode.Status200.statusCode) {
                 return true
             }
             val result: Result<String> = Result(
