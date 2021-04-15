61e71cf3518f07884cac3c680a7048a806ee11a1
Umb <1149558764@qq.com>
2021-04-15 17:10:36 +0800

    跨域发送cookie

--- src/main/kotlin/com/fake/information/sever/demo/Http/Interceptor/Interceptor.kt
+++ src/main/kotlin/com/fake/information/sever/demo/Http/Interceptor/Interceptor.kt
@@ -16,33 +16,38 @@ import javax.servlet.http.HttpServletResponse
 class Interceptor : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .exposedHeaders("access-control-allow-headers",
+            .exposedHeaders(
+                "access-control-allow-headers",
                 "access-control-allow-methods",
                 "access-control-allow-origin",
                 "access-control-max-age",
                 "Authorization",
-                "X-Frame-Options")
+                "X-Frame-Options"
+            )
             .allowCredentials(false).maxAge(3600);
     }
 
     @Override
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(SecurityInterceptor(redisTemplate))
-                .addPathPatterns("/**")
-                //排除拦截
-                .excludePathPatterns(arrayListOf(
-                        "/v1/login/**",
-                        "/v1/signUp/**",
-                        "/v1/signUp/email",
-                        "/v1/login/getPublicKey",
-                        "/v1/cdKey/verify",
-                        "/v1/login/verifyCode/{date}",
-                        "/v1/forward/",
-                        "/v1/forward/change",
-                        "/v1/forward/sendEmail",
-                        "/ImageEdge",
-                        "/ImageEdgeByte"
-                ))
+            .addPathPatterns("/**")
+            //排除拦截
+            .excludePathPatterns(
+                arrayListOf(
+                    "/v1/login/**",
+                    "/v1/signUp/**",
+                    "/v1/signUp/email",
+                    "/v1/login/getPublicKey",
+                    "/v1/cdKey/verify",
+                    "/v1/login/verifyCode/{date}",
+                    "/v1/forward/",
+                    "/v1/forward/change",
+                    "/v1/forward/sendEmail",
+                    "/ImageEdge",
+                    "/v1/FakeNewsVerify/",
+                    "/ImageEdgeByte"
+                )
+            )
         //拦截路径
 
     }
@@ -55,18 +60,18 @@ class Interceptor : WebMvcConfigurer {
 
 
         override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-            response.addHeader("Access-Control-Allow-Credentials","true")
+            response.addHeader("Access-Control-Allow-Credentials", "true")
             val session = request.session
             if (redisTemplate.getRedis(session.id) == StatusCode.Status200.statusCode) {
                 return true
             }
             val result: Result<String> = Result(
-                    success = false,
-                    code = StatusCode.Status302.statusCode,
-                    msg = "Bad Request",
-                    data = "Please Login"
+                success = false,
+                code = StatusCode.Status302.statusCode,
+                msg = "Bad Request",
+                data = "Please Login"
             )
-            response.status=StatusCode.Status302.statusCode
+            response.status = StatusCode.Status302.statusCode
             response.writer.print(result.toJson())
             return false
         }
